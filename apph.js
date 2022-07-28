35d2f523ef014efdb3c5879008c6bd107250746e
MrBien0405 <110025434+MrBien0405@users.noreply.github.com>
2022-07-28 21:13:42 +0700

    Add files via upload

---
+++ apph.js
@@ -0,0 +1,26 @@
+//Bài 1
+// let a = prompt("nhập điểm SV môn Vật Lý");
+// let b = prompt("nhập điểm SV môn Hóa Học");
+// let c = prompt("nhập điểm SV môn Sinh Học");
+// let tong = Number(a)+Number(b)+Number(c);
+// let đtb = (Number(a)+Number(b)+Number(c))/3;
+// alert(`Tổng điểm của SV là ${tong}`);
+// alert(`Điểm trung bình của SV là ${đtb}`);
+
+
+// Bài 2
+// let a = prompt("Nhập giá trị độ C");
+// let Fahrenheit = (9/5)*a+32
+// alert(`Giá trị độ F là ${Fahrenheit}`);
+
+
+//Bài 3: 
+// let r = prompt("nhập bán kính hình tròn");
+// let sqrt = r*r*Math.PI;
+// alert(`Diện tích hình tròn là ${sqrt}`);
+
+
+//Bài 4:
+let r = prompt("nhập bán kính hình tròn");
+let sqrt = 2*r*Math.PI;
+alert(`Chu vi hình tròn là ${sqrt}`);
